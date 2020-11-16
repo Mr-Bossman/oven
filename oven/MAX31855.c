7df02f7526c4843bea9fbe8beecc823ca472e969
Mr-Bossman <mr.bossman075@gmail.com>
2020-11-15 22:54:42 -0500

    temp works still needs better temp control and status

--- oven/MAX31855.c
+++ oven/MAX31855.c
@@ -4,19 +4,19 @@ uint16_t sence_temp = 0;
 uint16_t sence_temp_internal = 0;
 
 static uint32_t spi(){
-	PORTB &= ~(1 << 5);
+	PORTC &= ~(1 << 5);
 	_delay_ms(1);
 	uint32_t ret = 0;
 	for(uint8_t i = 0;i < 32; i++){
-		PORTB |= (1 << 3); // 100ns delays between each instruction
+		PORTE |= (1 << 2); // 100ns delays between each instruction
 		_delay_loop_1(10);
 		ret <<= 1;
-		ret |= (PINB >> 4) & 1;
-		PORTB &= ~(1 << 3);
+		ret |= (PINC >> 4) & 1;
+		PORTE &= ~(1 << 2);
 		_delay_loop_1(10);
 	}
 	_delay_ms(1);
-	PORTB |= (1 << 5);
+	PORTC |= (1 << 5);
 	_delay_ms(1);
 	return ret;
 }
